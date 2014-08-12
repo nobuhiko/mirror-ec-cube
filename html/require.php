d877a54e0c22a43f9b5403a1a43a93299f80cf27
のぶ <nobuhiko.kimoto@gmail.com>
2014-08-12 13:18:15 +0900

    ErrorException: Notice: ob_end_clean()

--- html/require.php
+++ html/require.php
@@ -33,7 +33,8 @@ if (!defined('ADMIN_FUNCTION') || ADMIN_FUNCTION !== true) {
 
 require_once HTML_REALDIR . 'define.php';
 if (ob_get_level() > 0 && ob_get_length() > 0) {
-    while (ob_end_clean());
+    //while (ob_end_clean());
+    ob_end_clean();
 }
 require_once HTML_REALDIR . HTML2DATA_DIR . 'require_base.php';
 
